62c1a022aa003f470b948be599052e302f8e4ee5
Michael Skirda <michael.skirda@gmail.com>
2020-03-29 05:45:24 +0300

    Color added. Now in color...

--- ytsc/youtuber.js
+++ ytsc/youtuber.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+const style = require('ansi-styles');
 const { Image } = require('image-js');
 
 (async () =>
@@ -8,94 +8,57 @@ const { Image } = require('image-js');
 	browser = await puppeteer.launch();
 	page = await browser.newPage();
 
-	url = "https://www.youtube.com/watch?v=9f6sNnjxeiE";
+	url = "https://www.youtube.com/watch?v=N6D6nL7-Ako";
 
 	await page.goto(url);
 
-	waitLoad = null;
+	await page.waitFor(1000);
 
-	// Wait loading and make screenshot
-	while(waitLoad == null)
-	{
-		try
-		{
-		waitLoad = await page.$('#logo');
-		await page.waitFor(1000);
-		}
-		catch
-		{
-			await page.screenshot({path: "Error.png" });
-			await console.log("Error");
-			await browser.close();
-		}
-	}
+	await page.click('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button');
 
 	element = await page.$('#movie_player > div.html5-video-container > video');
 
-	await page.waitFor(500)
-
-	await page.click('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button');
 	await page.click('#more');
+	await element.screenshot({path: "img/onload.png" });
+		let image = await Image.load('img/onload.png');
+		image = await image
+    		.resize({ width: 175 })
 
-	console.clear()
+    		image.save('resized.jpg');
 
-	
-	let grey;
+	let img = new Array(image.height);
+
+	for (x = 0; x < img.length; x++)
+	{
+  		img[x] = new Array(image.width - 1);
+	}
 
 	while(page.url() == url)
 	{
 
 		await element.screenshot({path: "img/onload.png" });
 		let image = await Image.load('img/onload.png');
-		grey = await image
-    		.grey()
-    		.resize({ width: 60 })
-    	grey.save('cat.png');
+		image = await image
+    		.resize({ width: 175 })
 
-    	await console.log(grey.getPixelXY(0, 0)[0]);
-
-    	let img = new Array(grey.height);
-	console.clear();
-	for (x = 0; x < img.length; x++)
+	for(i = 0; i < image.height; await i++)
 	{
-  		img[x] = new Array(grey.width);
-	}
-
-
-	for(i = 0; i < grey.width; await i++)
-	{
-		for(j = 0; j < grey.height; await j++)
+		for(j = 0; j < image.width; await j++)
 		{
-			await console.log(i + ' ' + j + ' ' + grey.getPixelXY(i, j))
-			color = await grey.getPixelXY(i, j);
-			if(color > 200)
-			{
-				img[j][i] = await '.';
-			}
-			else if(color > 150)
-			{
-				img[j][i] = await '/';
-			}
-			else if(color > 80)
-			{
-				img[j][i] = await '*';
-			}
-			else
-			{
-				img[j][i] = await '#';
-			}
+			img[i][j] = await style.bgColor.ansi.rgb(image.getPixelXY(j, i)[0], image.getPixelXY(j, i)[1], image.getPixelXY(j, i)[2]);
+			
+			img[i][j] += ' ';
 		}
 	}
 	console.clear();
-	for(i = 0; i < grey.width; await i++)
+	for(i = 0; i < image.height; await i++)
 	{
-		result = String(img[i]);
-		result = result.replace(',', '');
-		if(result != "undefined")
-		{
-			console.log(result);
-		}
+		result = await String(img[i]);
+		await console.log(result);
 	}
+
+	await console.log(style.bgColor.ansi.rgb(0, 0, 0));
+
 	}
 
 
