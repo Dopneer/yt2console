0fbafe6cbd1a180dd3ba1e5ca3b194c8cf95f5a5
Michael Skirda <michael.skirda@gmail.com>
2020-04-19 01:00:44 +0300

    windows fix

--- ytsc/youtuber.js
+++ ytsc/youtuber.js
@@ -2,31 +2,37 @@ const puppeteer = require('puppeteer');
 const style = require('ansi-styles');
 const readline = require('readline');
 const { Image } = require('image-js');
+
 (async () =>
 {
 	browser = await puppeteer.launch({ headless: true });
 	page = await browser.newPage();
 	url = "https://www.youtube.com/watch?v=N6D6nL7-Ako";
 	await page.goto(url);
-	await page.waitFor(1000);
+	await page.waitFor(2000);
+
 	await page.click('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button');
+
 	element = await page.$('#movie_player > div.html5-video-container > video');
-	await page.click('#more');
-	await element.screenshot({path: "img/onload.png" });
-		let image = await Image.load('img/onload.png');
+	await element.screenshot({path: "onload.png" });
+		let image = await Image.load('onload.png');
 		image = await image
+			.resize({ width: 200 })
     		image.save('resized.jpg');
 	let img = new Array(image.height);
 	for (x = 0; x < img.length; x++)
 	{
   		img[x] = new Array(image.width + 1);
 	}
+
+	// Main cicle
+
 	while(page.url() == url)
 	{
-		await element.screenshot({path: "img/onload.png" });
-		let image = await Image.load('img/onload.png');
+		await element.screenshot({path: "onload.png" });
+		let image = await Image.load('onload.png');
 		image = await image
-    		.resize({ width: 683 })
+    		.resize({ width: 200 })
 	for(i = 0; i < image.height; await i++)
 	{
 		for(j = 0; j < image.width - 1; await j++)
@@ -41,8 +47,11 @@ const { Image } = require('image-js');
 
 	result = await String(img);
 	await console.log(result);
-	
-	await page.waitFor(1500);
 	}
+
+	// End of main cicle
+
+
+
 	await browser.close();
 })();
