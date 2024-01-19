dab87de34918b8819442387eee9c71bee769c656
SkirdaMK <skirda.michael@gmail.com>
2024-01-19 09:57:44 +0300

    2024 fix

--- ytsc/youtuber.js
+++ ytsc/youtuber.js
@@ -1,26 +1,26 @@
-const puppeteer = require('puppeteer');
-const style = require('ansi-styles');
-const readline = require('readline');
-const { Image } = require('image-js');
+import *  as puppeteer from 'puppeteer' 
+import styles from 'ansi-styles'
+import * as readline from 'readline'
+import { Image } from 'image-js'
 
 (async () =>
 {
-	browser = await puppeteer.launch({ headless: true });
-	page = await browser.newPage();
-	url = "https://www.youtube.com/watch?v=N6D6nL7-Ako";
+	let browser = await puppeteer.launch({ headless: true });
+	let page = await browser.newPage();
+	let url = "https://www.youtube.com/watch?v=mjF1rmSV1dM";
 	await page.goto(url);
-	await page.waitFor(2000);
+	await page.waitForTimeout(2000);
 
 	await page.click('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button');
 
-	element = await page.$('#movie_player > div.html5-video-container > video');
+	let element = await page.$('#movie_player > div.html5-video-container > video');
 	await element.screenshot({path: "onload.png" });
 		let image = await Image.load('onload.png');
 		image = await image
-			.resize({ width: 200 })
+			.resize({ width: 300 })
     		image.save('resized.jpg');
 	let img = new Array(image.height);
-	for (x = 0; x < img.length; x++)
+	for (let x = 0; x < img.length; x++)
 	{
   		img[x] = new Array(image.width + 1);
 	}
@@ -33,11 +33,15 @@ const { Image } = require('image-js');
 		let image = await Image.load('onload.png');
 		image = await image
     		.resize({ width: 200 })
-	for(i = 0; i < image.height; await i++)
+	for(let i = 0; i < image.height; await i++)
 	{
-		for(j = 0; j < image.width - 1; await j++)
+		for(let j = 0; j < image.width - 1; await j++)
 		{
-			img[i][j] = await style.bgColor.ansi.rgb(image.getPixelXY(j, i)[0], image.getPixelXY(j, i)[1], image.getPixelXY(j, i)[2]);
+			img[i][j] = await styles.bgColor.ansi16m(
+
+				image.getPixelXY(j, i)[0],
+				image.getPixelXY(j, i)[1],
+				image.getPixelXY(j, i)[2]);
 			img[i][j] += await ' ';
 		}
 		img[i][image.width] = '\n';
@@ -45,7 +49,7 @@ const { Image } = require('image-js');
 	await console.clear();
 
 
-	result = await String(img);
+	let result = await String(img);
 	await console.log(result);
 	}
 
